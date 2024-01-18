d7fbefb8e9001fc71b276a3b4cc68a3e27ee3dab
Victor B. <contact@itsvic.dev>
2024-01-18 20:39:26 +0100

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,39 +1,36 @@
 {
 	description = "RGBDS";
 	inputs = {
-		nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+		nixpkgs.url = "github:NixOS/nixpkgs";
+		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = { self, nixpkgs }:
-		let
-			version = "0.7.0";
-			supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-			forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-			nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-		in {
-			packages = forAllSystems (system:
-				let
-					pkgs = nixpkgsFor.${system};
-				in {
-					default = pkgs.stdenv.mkDerivation {
-						pname = "rgbds";
-						inherit version;
-						src = pkgs.fetchFromGitHub {
-							owner = "gbdev";
-							repo = "rgbds";
-							rev = "v${version}";
-							sha256 = "sha256-aktKJlwXpHpjSFxoz5wZJPGWZIcn4ax5iBP0GQEux78=";
-						};
-						nativeBuildInputs = with pkgs; [
-							bison
-							cmake
-							pkg-config
-						];
-						buildInputs = with pkgs; [
-							zlib.dev
-							libpng.dev
-						];
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in {
+				packages.default = pkgs.stdenv.mkDerivation rec {
+					pname = "rgbds";
+					version = "0.7.0";
+					src = pkgs.fetchFromGitHub {
+						owner = "gbdev";
+						repo = "rgbds";
+						rev = "v${version}";
+						sha256 = "sha256-aktKJlwXpHpjSFxoz5wZJPGWZIcn4ax5iBP0GQEux78=";
 					};
-				});
-		};
+					nativeBuildInputs = with pkgs; [
+						bison
+						cmake
+						pkg-config
+					];
+					buildInputs = with pkgs; [
+						zlib.dev
+						libpng.dev
+					];
+				};
+			}
+		);
 }
